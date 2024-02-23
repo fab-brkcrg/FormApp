0068ddfc00d1a95d94084be53f58f85670c351e9
fab-brkcrg <b.cirag@fabay.com.tr>
2024-02-23 13:58:08 +0300

    first commit

---
+++ HrWebAPI-main/Program.cs
@@ -0,0 +1,48 @@
+using Dapper;
+using HRWebAPI.Services; //Entity yerine dapper kullan?yoruz
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+builder.Services.AddScoped<IDbService, DbService>();
+builder.Services.AddScoped<IUsersInformationService, UsersInformationService>();
+// Add services to the container.
+
+//builder.Services.AddCors();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("MyCorsPolicy");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseCors(a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
